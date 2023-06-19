4844e9a0a368b455fead447df3b8538afd741b4d
Wes Hendley <wrhendley@gmail.com>
2023-06-19 14:03:46 -0500

    Add preliminary code for #knight_moves

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,10 +1,50 @@
 require_relative 'board'
 
 class Knight
-  attr_accessor :coordinate
+  attr_accessor :coordinates
 
-  def initialize(coordinate)
-    @coordinate = coordinate
+  def initialize(board)
     @board = board
   end
+end
+
+public
+
+def knight_moves(start_coordinates, target_coordinates)
+  return start_coordinates if start_coordinates == target_coordinates
+
+  path_arr = [start_coordinates]
+  unless start_coordinates[0] >= @board.width - 1 || start_coordinates[1] >= @board.height - 1  # up right
+    path_arr << knight_moves([start_coordinates[0] + 1, start_coordinates[1] + 2], target_coordinates)
+    return path_arr if path_arr[path_arr.length - 1] == target_coordinates
+  end
+  unless start_coordinates[0] >= @board.width - 2 || start_coordinates[1] >= @board.height - 2  # right up
+    path_arr += knight_moves([start_coordinates[0] + 2, start_coordinates[1] + 1], target_coordinates)
+    return path_arr if path_arr[path_arr.length - 1] = target_coordinates
+  end
+  unless start_coordinates[0] >= @board.width - 2 || start_coordinates[1] <= 1                  # right down
+    path_arr += knight_moves([start_coordinates[0] + 2, start_coordinates[1] - 1], target_coordinates)
+    return path_arr if path_arr[path_arr.length - 1] = target_coordinates
+  end
+  unless start_coordinates[0] >= @board.width - 1 || start_coordinates[1] <= 0                  # down right
+    path_arr += knight_moves([start_coordinates[0] + 1, start_coordinates[1] - 2], target_coordinates)
+    return path_arr if path_arr[path_arr.length - 1] = target_coordinates
+  end
+  unless start_coordinates[0] <= 1 || start_coordinates[1] <= 0                                 # down left
+    path_arr += knight_moves([start_coordinates[0] - 1, start_coordinates[1] - 2], target_coordinates)
+    return path_arr if path_arr[path_arr.length - 1] = target_coordinates
+  end
+  unless start_coordinates[0] <= 0 || start_coordinates[1] <= 1                                 # left down
+    path_arr += knight_moves([start_coordinates[0] - 2, start_coordinates[1] - 1], target_coordinates)
+    return path_arr if path_arr[path_arr.length - 1] = target_coordinates
+  end
+  unless start_coordinates[0] <= 0 || start_coordinates[1] >= @board.height - 2                 # left up
+    path_arr += knight_moves([start_coordinates[0] - 2, start_coordinates[1] + 1], target_coordinates)
+    return path_arr if path_arr[path_arr.length - 1] = target_coordinates
+  end
+  unless start_coordinates[0] <= 1 || start_coordinates[1] >= @board.height - 1                 # up left
+    path_arr += knight_moves([start_coordinates[0] - 1, start_coordinates[1] + 2], target_coordinates)
+    return path_arr if path_arr[path_arr.length - 1] = target_coordinates
+  end
+
 end
